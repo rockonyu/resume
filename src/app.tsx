6d56e5bb632e4899fb9fa2cb9adb284905007b5a
austin <rockonyu@gmail.com>
2019-07-02 18:47:47 +0800

    更新 resume.json 與 AMP 頁面

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,25 @@
-import * as React from 'react';
-import { renderToStaticMarkup } from 'react-dom/server';
-import { Layout } from './client/components';
-import { App } from './client/App';
+import * as React from 'react'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { Layout } from './client/components'
+import { App } from './client/App'
+import * as result from './resume.json'
 
-const styles = require('./client/style.css').toString();
+const resume = result as any
+const styles = require('./client/style.css').toString()
 
-const Page = Layout(
-  {
-    title: (new Date).getFullYear() + ' 張瑀的個人履歷',
+const Page = Layout({
+    title: new Date().getFullYear() + ' 張瑀的個人履歷',
     body: renderToStaticMarkup(<App />),
-    styles: styles
-  }
-);
+    styles: styles,
+    description: resume.basics.description,
+})
 
- var fs = require('fs');
+var fs = require('fs')
 
- fs.writeFile("./index.html", Page, function(err) {
-  if(err) {
-      return console.log(err);
-  }
+fs.writeFile('./index.html', Page, function(err) {
+    if (err) {
+        return console.log(err)
+    }
 
-  console.log("The file was saved!");
-});
+    console.log('The file was saved!')
+})
--- src/client/components/Layout.ts
+++ src/client/components/Layout.ts
@@ -1,4 +1,4 @@
-const Layout = ({ title, body, styles }) => `
+const Layout = ({ title, body, description, styles }) => `
   <!doctype html>
   <html ⚡>
     <head>
@@ -7,15 +7,11 @@ const Layout = ({ title, body, styles }) => `
       <title>${title}</title>
       <link rel="canonical" href="http://rockonyu.github.io/resume">
       <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+      <meta name="description" content=${description}>
       <script type="application/ld+json">
         {
           "@context": "http://schema.org",
-          "@type": "NewsArticle",
-          "headline": "Open-source framework for publishing content",
-          "datePublished": "2015-10-07T12:02:41Z",
-          "image": [
-            "logo.jpg"
-          ]
+          "@type": "Person"
         }
       </script>
       <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
@@ -27,6 +23,6 @@ const Layout = ({ title, body, styles }) => `
       <div id="app">${body}</div>
     </body>
   </html>
-`;
+`
 
-export default Layout;
+export default Layout
