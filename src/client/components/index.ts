63122bc6d8addf64dd04221ad5579edfc5453e32
yuzhang <rockonyu@gmail.com>
2020-07-12 00:00:46 +0800

    feat: destroy all

--- src/client/components/index.ts
+++
@@ -1,6 +0,0 @@
-import Header from './Header';
-import Aside from './Aside';
-import Main from './Main';
-import Layout from './Layout';
-
-export { Header, Aside, Main, Layout };
--- src/client/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader';
-import App from './App';
-
-const MyApp = hot(module)(App);
-ReactDOM.render(<MyApp />, document.getElementById('app'));
