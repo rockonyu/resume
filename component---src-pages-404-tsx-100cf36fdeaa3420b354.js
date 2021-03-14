5322f950f70e540e420352a1851729c8877a7eb3
rockonyu <rockonyu@users.noreply.github.com>
2021-03-14 14:23:46 +0000

    deployed via Gatsby Publish Action 🎩 for 0669a8981ba231a0c5c50d17df1622d66c47707d

---
+++ component---src-pages-404-tsx-100cf36fdeaa3420b354.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(t,e,n){"use strict";n.r(e);n("q1tI");var u=n("Nhdc"),d=n("qKvR");e.default=function(){return Object(d.d)(u.c,null,Object(d.d)(u.d,{title:"404: Not found"}),Object(d.d)("h1",null,"NOT FOUND"),Object(d.d)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-100cf36fdeaa3420b354.js.map
